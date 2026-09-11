42bb0b13879b72bfd7d2c050ca2b5882def590ed
Nishma <nishma4164@gmail.com>
2026-09-11 15:40:54 +0530

    Delete reverse.java

--- reverse.java
+++
@@ -1,9 +0,0 @@
-public class reverse {
-    public static void main(String[] args) {
-        int[]a={2,3,6,8};
-     
-        for (int i=a.length-1;i>=0;i--){
-            System.out.println(a[i]);
-        }
-    }
-}
