a90d24d0fa41d92cc09eb808db1ebacf38c94968
Nishma <nishma4164@gmail.com>
2026-09-11 15:37:51 +0530

    Delete student.java

--- student.java
+++
@@ -1,8 +0,0 @@
-public class student {
-    student(){
-        System.out.println("student object created");
-    }
-    public static void main(String[] args) {
-        student s1=new student();
-    }
-}
